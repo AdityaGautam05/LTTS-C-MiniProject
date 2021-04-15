cd58df3a792e02c17bf03ab995b0a0376ec3c79f
AdityaGautam05 <gautamaditya99@gmail.com>
2021-04-15 22:01:26 +0530

    Update CBank_Header.c

--- CBank/3_Implementation/src/CBank_Header.c
+++ CBank/3_Implementation/src/CBank_Header.c
@@ -225,7 +225,7 @@ void compute_transaction(void)
         if (add.acc_no == transaction.acc_no)
         {
             test = 1;
-            if (strcmpi(add.acc_type, "fixed1") == 0 || strcmpi(add.acc_type, "fixed2") == 0 || strcmpi(add.acc_type, "fixed3") == 0)
+            if (strcmp(add.acc_type, "fixed1") == 0 || strcmp(add.acc_type, "fixed2") == 0 || strcmp(add.acc_type, "fixed3") == 0)
             {
                 printf("\a\a\a\n\n YOU CANNOT DEPOSIT OR WITHDRAW CASH IN FIXED ACCOUNTS !!!!!");
                 compute_delay(1000000000);
@@ -369,35 +369,35 @@ void see_details(void)
 
                 printf("\nAccount NO.:%d\nName:%s \nDOB:%d/%d/%d \nAge:%d \nAddress:%s \nCitizenship No:%s \nPhone number:%.0lf \nType Of Account:%s \nAmount deposited:$ %.2f \nDate Of Deposit:%d/%d/%d\n\n", add.acc_no, add.name, add.dob.month, add.dob.day, add.dob.year, add.age, add.address, add.citizenship, add.phone,
                        add.acc_type, add.amt, add.deposit.month, add.deposit.day, add.deposit.year);
-                if (strcmpi(add.acc_type, "fixed1") == 0)
+                if (strcmp(add.acc_type, "fixed1") == 0)
                 {
                     time = 1.0;
                     rate = 9;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\n You will get $%.2f as interest on %d/%d/%d", intrst, add.deposit.month, add.deposit.day, add.deposit.year + 1);
                 }
-                else if (strcmpi(add.acc_type, "fixed2") == 0)
+                else if (strcmp(add.acc_type, "fixed2") == 0)
                 {
                     time = 2.0;
                     rate = 11;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\n You will get $.%.2f as interest on %d/%d/%d", intrst, add.deposit.month, add.deposit.day, add.deposit.year + 2);
                 }
-                else if (strcmpi(add.acc_type, "fixed3") == 0)
+                else if (strcmp(add.acc_type, "fixed3") == 0)
                 {
                     time = 3.0;
                     rate = 13;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\n You will get $.%.2f as interest on %d/%d/%d", intrst, add.deposit.month, add.deposit.day, add.deposit.year + 3);
                 }
-                else if (strcmpi(add.acc_type, "saving") == 0)
+                else if (strcmp(add.acc_type, "saving") == 0)
                 {
                     time = (1.0 / 12.0);
                     rate = 8;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\n You will get $.%.2f as interest on %d of every month ", intrst, add.deposit.day);
                 }
-                else if (strcmpi(add.acc_type, "current") == 0)
+                else if (strcmp(add.acc_type, "current") == 0)
                 {
 
                     printf("\n\n You will get no interest \a\a");
@@ -411,41 +411,41 @@ void see_details(void)
         scanf("%s", &check.name);
         while (fscanf(ptr, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d", &add.acc_no, add.name, &add.dob.month, &add.dob.day, &add.dob.year, &add.age, add.address, add.citizenship, &add.phone, add.acc_type, &add.amt, &add.deposit.month, &add.deposit.day, &add.deposit.year) != EOF)
         {
-            if (strcmpi(add.name, check.name) == 0)
+            if (strcmp(add.name, check.name) == 0)
             {
                 system("cls");
                 test = 1;
                 printf("\n Account No.:%d\nName:%s \nDOB:%d/%d/%d \nAge:%d \nAddress:%s \nCitizenship No:%s \nPhone number:%.0lf \nType Of Account:%s \nAmount deposited:$%.2f \nDate Of Deposit:%d/%d/%d\n\n", add.acc_no, add.name, add.dob.month, add.dob.day, add.dob.year, add.age, add.address, add.citizenship, add.phone,
                        add.acc_type, add.amt, add.deposit.month, add.deposit.day, add.deposit.year);
-                if (strcmpi(add.acc_type, "fixed1") == 0)
+                if (strcmp(add.acc_type, "fixed1") == 0)
                 {
                     time = 1.0;
                     rate = 9;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\nYou will get $.%.2f as interest on %d/%d/%d", intrst, add.deposit.month, add.deposit.day, add.deposit.year + 1);
                 }
-                else if (strcmpi(add.acc_type, "fixed2") == 0)
+                else if (strcmp(add.acc_type, "fixed2") == 0)
                 {
                     time = 2.0;
                     rate = 11;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\nYou will get $.%.2f as interest on %d/%d/%d", intrst, add.deposit.month, add.deposit.day, add.deposit.year + 2);
                 }
-                else if (strcmpi(add.acc_type, "fixed3") == 0)
+                else if (strcmp(add.acc_type, "fixed3") == 0)
                 {
                     time = 3.0;
                     rate = 13;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\nYou will get $.%.2f as interest on %d/%d/%d", intrst, add.deposit.month, add.deposit.day, add.deposit.year + 3);
                 }
-                else if (strcmpi(add.acc_type, "saving") == 0)
+                else if (strcmp(add.acc_type, "saving") == 0)
                 {
                     time = (1.0 / 12.0);
                     rate = 8;
                     intrst = compute_interest(time, add.amt, rate);
                     printf("\n\nYou will get $.%.2f as interest on %d of every month", intrst, add.deposit.day);
                 }
-                else if (strcmpi(add.acc_type, "current") == 0)
+                else if (strcmp(add.acc_type, "current") == 0)
                 {
 
                     printf("\n\n You will get no interest \a\a");
